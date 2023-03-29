9a9cb217b5c9b27c418e69d01fa70372c0fc6c7e
Sergio V <sevipi.vicents@gmail.com>
2023-03-29 16:29:12 +0200

    Formulari de contacte amb les seves funcionalitats OK!

--- target/classes/static/js/form_contacte.js
+++ target/classes/static/js/form_contacte.js
@@ -1,28 +1,41 @@
+
+//Funcionalitat Formulari de Contacte
 $(document).ready(function () {
 
     var emailreg = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
 
-    $("#botoenviar").click(function (){
+    $("#name").blur(function(){
         $(".error").remove();
+    });
+
+    $("#email").blur(function(){
+        $(".error").remove();
+    });
+
+    $("#assumpte").blur(function(){
+        $(".error").remove();
+    });
+
+    $("#message").blur(function(){
+        $(".error").remove();
+    });
+
+    $("#botoenviar").click(function (){
         if( $("#name").val() == "" ){
             $("#name").focus().after("<span class='error'>Afegeix el seu nom</span>");
             return false;
         }else if( $("#email").val() == "" || !emailreg.test($("#email").val()) ) {
             $("#email").focus().after("<span class='error'>Afegeix un email correcte</span>");
             return false;
-        }else if( $("#asunto").val() == ""){
-            $("#asunto").focus().after("<span class='error'>Afegeix un assumpte</span>");
+        }else if( $("#assumpte").val() == ""){
+            $("#assumpte").focus().after("<span class='error'>Afegeix un assumpte</span>");
             return false;
         }else if( $("#message").val() == "" ) {
             $("#message").focus().after("<span class='error'>Afegeix un missatge</span>");
             return false;
-        }else {
-
-                $(".frmhide").css("display","none");
-
-
         }
+            $('#success').show();
+            return false;
     });
-
 });
 
